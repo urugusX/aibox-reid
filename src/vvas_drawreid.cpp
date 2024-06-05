59df1c2d581e113d2d7ac586056afcce207a2a70
urugusX <163839695+urugusX@users.noreply.github.com>
2024-06-05 15:49:41 +0700

    Update vvas_drawreid.cpp

--- src/vvas_drawreid.cpp
+++ src/vvas_drawreid.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
