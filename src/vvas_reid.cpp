f9155e99fd66dbf4e594dcb5db3fb052776d6ca7
urugusX <163839695+urugusX@users.noreply.github.com>
2024-06-20 16:48:26 +0700

    Update vvas_reid.cpp

--- src/vvas_reid.cpp
+++ src/vvas_reid.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 #include <gst/vvas/gstinferencemeta.h>
 #include <vvas/vvas_kernel.h>
 #include <stdint.h>
